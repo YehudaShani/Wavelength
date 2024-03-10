2b93d5e242226b0079f2e8799c803009b068bb91
yehuda shani <yehudashani88@gmail.com>
2024-03-10 11:15:02 +0200

    changed dependencies, added target-guess, prob better without it

--- lib/screens/game_screen.dart
+++ lib/screens/game_screen.dart
@@ -1,16 +1,46 @@
-import 'package:flutter/material.dart';
+import 'dart:math';
 
-class GameScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:wavelength/widgets/question.dart';
+import 'package:wavelength/questions.dart';
+import 'package:wavelength/widgets/target_guess.dart';
+
+Map<String, dynamic> questionsData = wavelengthData;
+
+class GameScreen extends StatefulWidget {
   const GameScreen({super.key});
 
   @override
+  State<GameScreen> createState() => _GameScreenState();
+}
+
+class _GameScreenState extends State<GameScreen> {
+  var guess = 0;
+  var target = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    target = Random().nextInt(10) + 1;
+  }
+
+  void updateGuess(int newGuess) {
+    setState(() {
+      guess = newGuess;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [
-        Text('The question'),
-        Text('the picture'),
-        Text('submit button'),
-      ],
+    return Scaffold(
+      body: Column(
+        children: [
+          SizedBox(height: 60),
+          QuestionWidget(category: "Pets", subCategory: "Cuteness"),
+          Text('The guess is: $guess'),
+          Text('submit button'),
+        ],
+      ),
     );
   }
 }
